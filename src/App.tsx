8f94567052bc76584c41798a8ba86ab87dcfef0f
Simon DELAUNAY <simdelaunay@gmail.com>
2025-02-11 09:48:54 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-red-100 container h-100"></div>
+    </div>
+  );
+};
+
+export default App;
